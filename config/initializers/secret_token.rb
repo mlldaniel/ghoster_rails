c423e6b5fa3a8b7a5004442dc43c70fc56838758
mlldaniel <mlldaniel@naver.com>
2013-01-14 07:22:35 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GhosterRails::Application.config.secret_token = '2b2aef583336a746b75d99f038c58fdb2f6c242d75399541ee32130ebe04f97d366526ca0b560fb4679ea4c9e5bccaccb79843006a0cf9eef292083af7fc9372'
